b82d2a6103f0742ff7221d0b8c30402b66e59b4f
amany <amanyaso123@gmail.com>
2023-06-05 19:02:28 +0300

    add auth apis

---
+++ lib/general/models/notifications_model/notification_model.g.dart
@@ -0,0 +1,49 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'notification_model.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+NotificationModel _$NotificationModelFromJson(Map<String, dynamic> json) =>
+    NotificationModel(
+      id: json['id'] as String?,
+      type: json['type'] as String?,
+      title: json['title'] as String?,
+      body: json['body'] as String?,
+      data: json['data'] == null
+          ? null
+          : Data.fromJson(json['data'] as Map<String, dynamic>),
+      status: json['status'] as String?,
+      time: json['time'] as String?,
+    );
+
+Map<String, dynamic> _$NotificationModelToJson(NotificationModel instance) =>
+    <String, dynamic>{
+      'id': instance.id,
+      'type': instance.type,
+      'title': instance.title,
+      'body': instance.body,
+      'data': instance.data,
+      'status': instance.status,
+      'time': instance.time,
+    };
+
+Data _$DataFromJson(Map<String, dynamic> json) => Data(
+      id: json['id'] as int?,
+      amount: json['amount'] as String?,
+      type: json['type'] as String?,
+      gateway: json['gateway'] as String?,
+      status: json['status'] as String?,
+      onlinePaymentId: json['online_payment_id'] as String?,
+    );
+
+Map<String, dynamic> _$DataToJson(Data instance) => <String, dynamic>{
+      'id': instance.id,
+      'amount': instance.amount,
+      'type': instance.type,
+      'gateway': instance.gateway,
+      'status': instance.status,
+      'online_payment_id': instance.onlinePaymentId,
+    };
---
+++ lib/general/models/user_model/user_type.g.dart
@@ -0,0 +1,17 @@
+// GENERATED CODE - DO NOT MODIFY BY HAND
+
+part of 'user_type.dart';
+
+// **************************************************************************
+// JsonSerializableGenerator
+// **************************************************************************
+
+UserType _$UserTypeFromJson(Map<String, dynamic> json) => UserType(
+      id: json['id'] as int?,
+      name: json['name'] as String?,
+    );
+
+Map<String, dynamic> _$UserTypeToJson(UserType instance) => <String, dynamic>{
+      'id': instance.id,
+      'name': instance.name,
+    };
